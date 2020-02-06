fd3b8c266805692dc495c57668d4b1ceba7a4cd4
bertato <benjamin.bertato@orange.fr>
2020-02-06 14:01:42 +0100

    Choix par nom

--- Exploration.c
+++ Exploration.c
@@ -1,19 +1,19 @@
 #include <stdio.h>
 #include <string.h>
 
-int destination = 0;
+char destination[100];
 
 int main (){
 
 
-printf ("Quelle destination voulez vous choisir ? Forgefer (1) ou Orgrimmar (2) ?\n");
-scanf ("%d",&destination);
+printf ("Quelle destination voulez vous choisir ? Forgefer ou Orgrimmar ?\n");
+scanf("%[^\n]",destination);
 
-	if (destination == 1){
+	if ( strcmp (destination, "Forgefer") == 0){
 	printf ("Vous vous rendez donc a Forgefer\n");
 	}
 
-	else if (destination == 2){
+	else if ( strcmp (destination, "Orgrimmar") == 0){
 	printf ("Vous vous rendre donc a Orgrimmar");
 	}
 	
