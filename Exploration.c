7d50b4d3820ecedc50feeb53b5ef56ddf3e12507
bertato <benjamin.bertato@orange.fr>
2020-02-06 15:53:52 +0100

    fonction deplacement opti

--- Exploration.c
+++ Exploration.c
@@ -1,11 +1,14 @@
 #include <stdio.h>
 #include <string.h>
 
+
+char destination[100];
+
 struct Lieu {
 	char nom[100];
 	char description[100];
 	int  difficulte;
-	char id[100];
+	int id[2];
 	
 };
 typedef struct Lieu lieu;
@@ -14,27 +17,28 @@ typedef struct Lieu lieu;
 int deplacement (lieu lieu1,lieu lieu2,lieu lieu3){
 	
 	char destination[100];
+	int id = 0;
 	scanf ("%s",&destination);
 
 	if ( strcmp (destination, "Forgefer") == 0){
 	printf ("Vous vous rendez donc a %s\n", lieu1.nom);
 	printf ("%s\n",lieu1.description);
-	printf ("Quelle destination voulez vous choisir ? %s ou %s ?\n", lieu2.nom, lieu3.nom);
-	scanf ("%s",destination); 
+	printf ("Quelle destination voulez vous choisir ?  Hurlevent (1) ou Gnomeregan (2) ?\n");
+	scanf ("%d",&id); 
 	}
 		
 	if ( strcmp (destination, "Orgrimmar") == 0){
 	printf ("Vous vous rendez donc a %s\n", lieu2.nom);
 	printf ("%s\n",lieu2.description);
-	printf ("Quelle destination voulez vous choisir ?  %s ou %s ?\n", lieu1.nom, lieu3.nom);
-	scanf ("%s",destination); 
+	printf ("Quelle destination voulez vous choisir ?  Durotar (3) ou Tarides (4) ?\n");
+	scanf ("%d",&id);  
 	}
 		
 	else if ( strcmp (destination, "Dalaran") == 0){
 	printf ("Vous vous rendez donc a %s\n", lieu3.nom);
 	printf ("%s\n",lieu3.description);
-	printf ("Quelle destination voulez vous choisir ?  %s ou %s ?\n", lieu1.nom, lieu2.nom);
-	scanf ("%s",destination); 
+	printf ("Quelle destination voulez vous choisir ?  Suramar (6) ou ValShara (7) ?\n");
+	scanf ("%d",&id); 
 	}
 	
 }
@@ -42,9 +46,9 @@ int deplacement (lieu lieu1,lieu lieu2,lieu lieu3){
 int main (){
 	
 
-	lieu lieu1 ={ "Forgefer", "Capitale des nains" , 10, "Hurlevent"};
-	lieu lieu2 ={ "Orgrimmar", "Capitale des orc" , 15, "Durotar"};
-	lieu lieu3 ={ "Dalaran", "Capitale neutre" , 19, "Suramar"};
+	lieu lieu1 ={ "Forgefer", "Capitale des nains" , 10,{1,2}};
+	lieu lieu2 ={ "Orgrimmar", "Capitale des orc" , 15, {4,5}};
+	lieu lieu3 ={ "Dalaran", "Capitale neutre" , 19, {6,7}};
 	
 	printf ("Ou voulez vous aller ? %s ou %s ou %s ?\n", lieu1.nom, lieu2.nom, lieu3.nom);
 	
